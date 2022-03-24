59af8fe2bd369631370b01ceaf3789baef439bad
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-03-24 23:52:52 +0600

    init

--- main.py
+++ main.py
@@ -2,31 +2,49 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.uix.widget import Widget
+from kivymd.uix import MDAdaptiveWidget
 from kivymd.uix.label import MDLabel
 from kivymd.uix.button import MDRoundFlatButton
 
-from kivymd.uix.boxlayout import MDBoxLayout as bl
-from kivymd.uix.anchorlayout import AnchorLayout as al
+from kivymd.uix.boxlayout import BoxLayout as bl
+from kivy.uix.anchorlayout import AnchorLayout as al
+from kivy.uix.gridlayout import GridLayout as gl
+from kivy.uix.stacklayout import StackLayout as sl
 
 
 
-class MDwidget(al):
-	pass
+class MDAnchorLayout(al, MDAdaptiveWidget):
+    pass
 
-class box_layout(bl):
+class MDGridLayout(gl, MDAdaptiveWidget):
+    pass
+
+# class MDBoxLayout(bl, MDAdaptiveWidget):
+# 	b1=MDRoundFlatButton(text='A button')
+# 	b2=MDRoundFlatButton(text='b button')
+# 	b3=MDRoundFlatButton(text='c button')
+
+# 	self.add_widget(b1)
+# 	self.add_widget(b2)
+# 	self.add_widget(b3)
+# 	pass
+
+class Main_layout(gl,MDAdaptiveWidget): # GridLayout
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
 		#b1=BaseRoundButton(text='A button')
 		#b2=BaseRoundButton(text='b button')
-		b3=MDRoundFlatButton(text='c button')
+		#b3=MDRoundFlatButton(text='c button')
 
 		#self.add_widget(b1)
 		#self.add_widget(b2)
-		self.add_widget(b3)
+		#self.add_widget(b3)
 		
 		
 
 class red_app(MDApp):
+	def build(self):
+		return Main_layout()
 	pass
 
 #def build(self):
