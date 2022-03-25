92f0762d744741e98b283b5db1ac54ef745a34b1
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-03-25 23:45:01 +0600

    init

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
-#from kivy.app import App
+from kivy.app import App
 from kivy.lang import Builder
-from kivymd.app import MDApp
-from kivy.uix.widget import Widget
-from kivymd.uix import MDAdaptiveWidget
-from kivymd.uix.label import MDLabel
-from kivymd.uix.button import MDRoundFlatButton
-
-from kivymd.uix.boxlayout import BoxLayout as bl
 from kivy.uix.anchorlayout import AnchorLayout as al
+from kivy.uix.boxlayout import BoxLayout as bl
 from kivy.uix.gridlayout import GridLayout as gl
 from kivy.uix.stacklayout import StackLayout as sl
+from kivy.uix.widget import Widget
+from kivymd.app import MDApp
+from kivymd.uix import MDAdaptiveWidget
+from kivymd.uix.screen import MDScreen
+
+from kivymd.uix.button import (MDFlatButton, MDRectangleFlatButton,
+                               MDRoundFlatButton, MDTextButton)
+from kivymd.uix.label import MDLabel
 
 
-
-class MDAnchorLayout(al, MDAdaptiveWidget):
+class RootScreen(MDScreen):
     pass
 
-class MDGridLayout(gl, MDAdaptiveWidget):
+
+class nav_box(gl, MDAdaptiveWidget):
     pass
+
+
+class box_layout(bl):
+    pass
+
+
+class top_bar(al, MDAdaptiveWidget):
+    pass
+
+
+class tab_button(sl, MDAdaptiveWidget):
+    pass
+
+
+class header(gl, MDAdaptiveWidget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        l = MDLabel(text='A')
+        self.add_widget(l)
+        pass
+
 
 # class MDBoxLayout(bl, MDAdaptiveWidget):
 # 	b1=MDRoundFlatButton(text='A button')
@@ -29,27 +52,26 @@ class MDGridLayout(gl, MDAdaptiveWidget):
 # 	self.add_widget(b3)
 # 	pass
 
-class Main_layout(gl,MDAdaptiveWidget): # GridLayout
-	def __init__(self, **kwargs):
-		super().__init__(**kwargs)
-		#b1=BaseRoundButton(text='A button')
-		#b2=BaseRoundButton(text='b button')
-		#b3=MDRoundFlatButton(text='c button')
+class Main_layout(bl, MDAdaptiveWidget):  # GridLayout
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    # b1=BaseRoundButton(text='A button')
+    # b2=BaseRoundButton(text='b button')
+    # b3=MDRoundFlatButton(text='c button')
 
-		#self.add_widget(b1)
-		#self.add_widget(b2)
-		#self.add_widget(b3)
-		
-		
+    # self.add_widget(b1)
+    # self.add_widget(b2)
+    # self.add_widget(b3)
+
 
 class red_app(MDApp):
-	def build(self):
-		return Main_layout()
-	pass
+    def build(self):
+        return Builder.load_file("red_app.kv")
 
-#def build(self):
+
+# def build(self):
 #  return Label(text='Hello world',font_size=45)
- 
+
 
 if __name__ == '__main__':
     red_app().run()
